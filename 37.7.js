b2e9b9909c0a458b540d0c264dbbbdd9b0d7d1b3
rabbanictgbd <rabbanictgbd@gmail.com>
2025-04-25 15:46:37 +0600

    p

---
+++ 37.7.js
@@ -0,0 +1,10 @@
+
+const storageAdd = () => {
+    const idValue= document.getElementById("id").value
+const nameValue=    document.getElementById("name").value
+    console.log(idValue)
+console.log(nameValue)
+}
+
+console.log(idValue)
+console.log(nameValue)
